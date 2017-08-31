050f1968970f97da66bb157b26c36f75244dc743
keepupfor <namexd@163.com>
2017-08-31 16:07:35 +0800

    完成用户注册

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function create()
     {
         return view('users.create');
+    }
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+    public function store(Request $request)
+    {
+        $rules=[
+            'name'=>'required|max:25',
+            'email'=>'email|required|unique:users',
+            'password'=>'required|min:6|confirmed'
+        ];
+        $this->validate($request, $rules);
+        $user=User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password),
+        ]);
+        // Auth::login($user);
+        session()->flash('success','欢迎您的到来！');
+        return redirect()->route('users.show',[$user]);
     }
 }
---
+++ resources/views/shared/_user_info.blade.php
@@ -0,0 +1,5 @@
+<a href="{{route('users.show',$user->id)}}}">
+<img src="{{$user->gravatar()}}" alt="">
+</a>
+<h1>{{ $user->name }}
+</h1>
