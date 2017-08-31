16c2d3774c195e84db5d5bef2701eb152d0c963a
keepupfor <namexd@163.com>
2017-08-31 09:34:10 +0800

    完成首页样式和路由

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -8,15 +8,15 @@ class StaticPagesController extends Controller
 {
     public function home()
     {
-      return view('static_pages.home');
+        return view('static_pages.home');
     }
 
     public function help()
     {
-      return view('static_pages.help');
+        return view('static_pages.help');
     }
     public function about()
     {
-      return view('static_pages.about');
+        return view('static_pages.about');
     }
 }
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,10 +1,13 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    @yeild('content')
-  </body>
-</html>
+<header class="navbar navbar-fixed-top navbar-inverse">
+  <div class="container">
+    <div class="col-md-offset-1 col-md-10">
+      <a href="{route('home')}}" id="logo">Sample App</a>
+      <nav>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="{{route('help')}}">帮助</a></li>
+          <li><a href="{{route('signup')}}">登录</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</header>
