a8a2156cb882f85610f472b1a6a273f71847cd36
keepupfor <namexd@163.com>
2017-09-01 16:18:10 +0800

    完成用户编辑，更新，列表，删除操作

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,13 @@ use Illuminate\Support\Facades\Auth;
 
 class SessionsController extends Controller
 {
+        public function __construct()
+        {
+            $this->middleware('guest',[
+                'only'=>['create'],
+            ]);
+        }
+
     public function create()
     {
         return view('sessions.create');
@@ -14,26 +21,24 @@ class SessionsController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate($request,[
-            'email'=>'required|email|max:255',
-            'password'=>'required|between:6,15'
+        $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required|between:6,15'
         ]);
-      $condition=['email'=>$request->email,'password'=>$request->password];
-      if (Auth::attempt($condition,$request->has('remember'))){
-          session()->flash('success','欢迎回来');
-          return redirect()->route('users.show',[Auth::user()]);
-      }
-      else
-      {
-          session()->flash('danger','登录失败,邮箱或密码不正确');
-          return redirect()->back();
-      }
+        $condition = ['email' => $request->email, 'password' => $request->password];
+        if (Auth::attempt($condition, $request->has('remember'))) {
+            session()->flash('success', '欢迎回来');
+            return redirect()->intended(route('users.show', [Auth::user()]));
+        } else {
+            session()->flash('danger', '登录失败,邮箱或密码不正确');
+            return redirect()->back();
+        }
     }
 
     public function destroy()
     {
-      Auth::logout();
-      session()->flash('warning','您已退出!');
-      return redirect()->route('login');
+        Auth::logout();
+        session()->flash('warning', '您已退出!');
+        return redirect()->route('login');
     }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,20 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'except'=>['create','show','store','index']
+        ]);
+        $this->middleware('guest',[
+            'only'=>['create']
+        ]);
+    }
+    public function index()
+    {
+        $users=User::paginate(20);
+        return view('users.index',compact('users'));
+    }
     public function create()
     {
         return view('users.create');
@@ -33,5 +47,34 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success', '欢迎您的到来！');
         return redirect()->route('users.show', [$user]);
+    }
+    public function edit(User $user)
+    {
+        $this->authorize('update',$user);
+        return view('users.edit',compact('user'));
+    }
+    public function update(User $user,Request $request)
+    {
+        $rules = [
+            'name' => 'required|max:25',
+            'password' => 'nullable|min:6|confirmed'
+        ];
+        $this->validate($request, $rules);
+        $this->authorize('update',$user);
+        $data=[];
+        $data['name']=$request->name;
+        if ($request->password){
+            $data['password']=bcrypt($request->password);
+        }
+        $user->update($data);
+        session()->flash('success','修改个人信息成功');
+        return redirect()->route('users.show',$user->id);
+    }
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy',$user);
+        $user->delete();
+        session()->flash('success','删除成功!');
+        return redirect()->route('users.index');
     }
 }
