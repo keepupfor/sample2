db20fc7435775b8380c09fd1826623cca94b217b
keepupfor <namexd@163.com>
2017-09-01 11:19:44 +0800

    完成登录注销

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,25 +11,27 @@ class UsersController extends Controller
     {
         return view('users.create');
     }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
+
     public function store(Request $request)
     {
-        $rules=[
-            'name'=>'required|max:25',
-            'email'=>'email|required|unique:users',
-            'password'=>'required|min:6|confirmed'
+        $rules = [
+            'name' => 'required|max:25',
+            'email' => 'email|required|unique:users',
+            'password' => 'required|min:6|confirmed'
         ];
         $this->validate($request, $rules);
-        $user=User::create([
-            'name'=>$request->name,
-            'email'=>$request->email,
-            'password'=>bcrypt($request->password),
-        ]);
-        // Auth::login($user);
-        session()->flash('success','欢迎您的到来！');
-        return redirect()->route('users.show',[$user]);
+        $user = User::create(array(
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ));
+        Auth::login($user);
+        session()->flash('success', '欢迎您的到来！');
+        return redirect()->route('users.show', [$user]);
     }
 }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
-    <link rel="stylesheet" href="/css/app.css">
+     <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
 @include('layouts._header')
 
 <div class="container">
-  <div class="col-md-offset-1 col-md-10">
-    @include('shared._messages')
-    @yield('content')
+     <div class="col-md-offset-1 col-md-10">
+          @include('shared._messages')
+          @yield('content')
 
-    @include('layouts._footer')
+          @include('layouts._footer')
 
+     </div>
 </div>
-</div>
+<script src="/js/app.js"></script>
 </body>
 </html>
