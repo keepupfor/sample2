c21e21ad8622293a2c18f7a178577fdbda73b7af
keepupfor <namexd@163.com>
2017-09-04 16:24:09 +0800

    添加关注

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-    protected $tablename='users';
+    protected $tablename = 'users';
     /**
      * The attributes that are mass assignable.
      *
@@ -27,28 +27,66 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
     public static function boot()
     {
         parent::boot();
-        static::creating(function ($user){
-            $user->activation_token=str_random(30);
+        static::creating(function ($user) {
+            $user->activation_token = str_random(30);
         });
     }
-    public function gravatar($size='100')
+
+    public function gravatar($size = '100')
     {
-        $hash=md5(strtolower(trim($this->attributes['email'])));
+        $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://gravatar.com/avatar/$hash?s=$size";
     }
+
     public function sendPasswordResetNotification($token)
     {
         $this->notify(new ResetPassword($token));
     }
+
     public function statuses()
     {
         return $this->hasMany(Status::class);
     }
+
     public function feed()
     {
-        return $this->statuses()->orderBy('created_at','desc');
+        $user_ids = Auth::user()->followings->pluck('id')->toArray();
+        array_push($user_ids, Auth::user()->id);
+        return Status::whereIn('user_id', $user_ids)->orderBy('created_at', 'desc');
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
     }
 }
