ad9a3feeae180a5fb629835243334c0fb6473d3d
keepupfor <namexd@163.com>
2017-09-04 16:50:08 +0800

    Auth路径

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use App\Notifications\ResetPassword;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use Auth;
 class User extends Authenticatable
 {
     use Notifiable;
