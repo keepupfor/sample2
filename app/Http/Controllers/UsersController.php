a1464227b32f5c719bc429cb1f330ad458c41576
keepupfor <namexd@163.com>
2017-09-04 14:42:46 +0800

    添加用户微博

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -31,7 +31,7 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
-        $statuses=$user->statuses()->orderBy('created_at','desc');
+        $statuses=$user->statuses()->orderBy('created_at','desc')->paginate(30);
         return view('users.show', compact('user','statuses'));
     }
 
